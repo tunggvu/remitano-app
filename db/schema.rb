ebcf20dd57581378132e8de609ea09e29f239c2d
tunggvu <vutungtn95@gmail.com>
2019-08-22 13:26:59 +0700

    create homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_080907) do
+ActiveRecord::Schema.define(version: 2019_08_21_150858) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "url"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_08_21_080907) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "movies", "users"
 end
